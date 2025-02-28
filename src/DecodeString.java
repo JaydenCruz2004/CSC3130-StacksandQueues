d8162b9ef13b9156c39d0e39bcc2198192b99676
Jayden <jaydencruz824@gmail.com>
2025-02-28 13:30:19 -0600

    implemented DecodeString.java and added to Main.java

---
+++ src/DecodeString.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class DecodeString {
+    public static String decodeString(String s) {
+        Stack<String> stackStr = new Stack<>();
+        Stack<Integer> stackNum = new Stack<>();
+        StringBuilder currStr = new StringBuilder();
+        int currNum = 0;
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                currNum = currNum * 10 + (c - '0');
+            } else if (c == '[') {
+                stackStr.push(currStr.toString());
+                stackNum.push(currNum);
+                currStr = new StringBuilder();
+                currNum = 0;
+            } else if (c == ']') {
+                StringBuilder temp = new StringBuilder(stackStr.pop());
+                int num = stackNum.pop();
+                for (int i = 0; i < num; i++) {
+                    temp.append(currStr.toString());
+                }
+                currStr = temp;
+            } else {
+                currStr.append(c);
+            }
+        }
+
+        return currStr.toString();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        //Question 4:(code) Balanced Brackets [25 points]
+
+        //Question 4: (code) Balanced Brackets [25 points]
+        System.out.println("Question 4: Balanced Brackets");
         BalancedBrackets balancedBrackets = new BalancedBrackets();
         System.out.println(BalancedBrackets.isBalanced("{[()]}"));
         System.out.println(BalancedBrackets.isBalanced("{[(])}"));
         System.out.println(BalancedBrackets.isBalanced("{{[[(())]]}}"));
+
+        //Question 5: (code) Decode String [25 points]
+        System.out.println("\nQuestion 5: Decode String");
+        DecodeString decodeString = new DecodeString();
+        System.out.println(DecodeString.decodeString("3[a]2[bc]"));
+        System.out.println(DecodeString.decodeString("3[a2[c]]"));
+        System.out.println(DecodeString.decodeString("2[abc]3[cd]ef"));
+
 
     }
 }
